b30cf06b4a5b077cdc2571585448bb078b3cd678
Marina Ghattas <marina.ghattas@f1006-11-34159.bmcc.cuny.edu>
2015-10-19 14:27:56 -0400

    added more info

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
 
-
   strokeWeight(1);
   stroke('black');
-  fill('BurlyWood');
-  rect(50,48,125,20);
+  fill('lightblue');
+  rect(65,70,125-30,150);
+
+
+}
