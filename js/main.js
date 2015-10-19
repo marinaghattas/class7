e36ab82ac30ebf60ab85fc2c8b34090516fb0e05
Marina Ghattas <marina.ghattas@f1006-11-34159.bmcc.cuny.edu>
2015-10-19 16:24:21 -0400

    loops, grid

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
-
-  drawWindow (50,50); // call the function
-  drawWindow (200,50); // call the function
-
-
+//
+//  drawWindow (50,50); // call the function
+//  drawWindow (200,50); // call the function
 
 
-    for(var i=0; i<150; i++){
-        console.log(i);
-        drawWindow(50*i,50);
-}
 }
 
 function draw(){
-}
+    var x = 0;
+    var y = 0;
+    var gridWidth = 900;
+    for(var i=0; i<100; i++){
+        console.log(i);
+        drawWindow(x,y);
+        //increment our x
+        x += 120; //same as x = x + 120;
+        //chev=ck if x is past the drid width
+        if(x > gridWidth){
+            //set X back to the beginning
+            x=0;
+            //move y down
+            y += 270;
+
+        } // end if
+    } //end for loop
+} //end setup
 
 function drawWindow(startX,startY){
     strokeWeight(1);
     stroke('black');
     fill('lightpink');
     rect(startX,startY,100,250);
+
+
 
 
 }
